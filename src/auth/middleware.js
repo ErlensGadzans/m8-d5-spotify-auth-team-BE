1e69f7e721489d3ea2bfb14b7a3a9a0814498a10
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-02-19 15:15:02 +0200

    env file is updated working from atlas

--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -1,4 +1,4 @@
-const { verifyJWT } = require("../auth");
+const { verifyJWT } = require("../auth/index");
 const UserModel = require("../users/schema");
 
 const authorize = async (req, res, next) => {
