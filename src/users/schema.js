2e34c135537b8657d974c17fb880fafbbf2ff4fb
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-02-19 14:21:15 +0200

    login is fixed

--- src/users/schema.js
+++ src/users/schema.js
@@ -2,25 +2,18 @@ const { Schema, model } = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const UserSchema = new Schema({
-  email: {
-    email: String,
-    // required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-  },
+  email: String,
+  password: String,
 });
 
-UserSchema.statics.findByCredentials = async function (email, plainPW) {
+UserSchema.statics.findByCredentials = async function (email, password) {
   const user = await this.findOne({ email }); //FIRST LOOKING FOR USERNAME IN LIST. "THIS" IS SEARCHING FROM SCHEMA
 
   if (user) {
     //IF THERE IS A USER, THEN ARE LOOKIN FOR PASWORD TO MATCH
-    const isMatch = await bcrypt.compare(plainPW, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
     if (isMatch) return user;
-    else return null;
+    // else return null;
   } else {
     return null;
   }
