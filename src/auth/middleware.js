b4a9b693ae297c5eb6bdfa8cb8f3c2fdeaf7a570
HolaSoyGonzalo <72199035+HolaSoyGonzalo@users.noreply.github.com>
2021-02-19 14:54:12 +0100

    Update middleware.js

--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -4,9 +4,7 @@ const UserModel = require("../users/schema");
 const authorize = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    console.log(token);
     const decoded = await verifyJWT(token);
-    console.log(decoded);
     const user = await UserModel.findOne({ _id: decoded._id });
 
     if (!user) {
