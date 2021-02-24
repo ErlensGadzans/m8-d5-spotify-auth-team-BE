3613159092358d234ae9c67e81b35f2cbe3b6dfe
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-02-24 18:13:19 +0200

    google login fixet

--- src/auth/oauth.js
+++ src/auth/oauth.js
@@ -1,52 +1,53 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-UserSchema = require("../users/schema")
-const {authenticate} = require("../auth/index")
-
+UserSchema = require("../users/schema");
+const { authenticate } = require("../auth/index");
 
 // Use the GoogleStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
 passport.use(
+  "google",
   new GoogleStrategy(
     {
-      clientID: pocess.env.GOOGLE_CLIENT_ID,
+      clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRECT,
       callbackURL: process.env.REDIRECT_URL,
     },
-    function (accessToken, refreshToken, profile, done) {
+    async function (accessToken, refreshToken, profile, done) {
       // console.log(profile);
-     const user = await UserSchema.findOne({googleId: profile.id})
-try{
 
-  if (!user) {
-    const newUser = new UserSchema({
-      googleId: profile.id,
-      name: profile.name.givenName,
-      img: profile.photos.value,
-      email: profile.emails[0].value,
-    });
-    const savedUser = await newUser.save();
-    const accessToken = await authenticate(savedUser);
-    done(null, { newUser, accessToken }); //NULL NOT ERROR SITUATION
-  } else {
-    //IF GOOGLE USER EXISTS JUST GENERATE TOKENS FOR HIM
-    const accessToken = await authenticate(user);
-    done(null, { user, accessToken });
-  }
-} catch (error) {
-  console.log(error);
-  // next(error);
-}
-}
-)
+      try {
+        const user = await UserSchema.findOne({ googleId: profile.id });
+
+        if (!user) {
+          const newUser = new UserSchema({
+            googleId: profile.id,
+            name: profile.name.givenName,
+            img: profile.photos.value,
+            email: profile.emails[0].value,
+          });
+          const savedUser = await newUser.save();
+          const accessToken = await authenticate(savedUser);
+          done(null, { newUser, accessToken }); //NULL NOT ERROR SITUATION
+        } else {
+          //IF GOOGLE USER EXISTS JUST GENERATE TOKENS FOR HIM
+          const accessToken = await authenticate(user);
+          done(null, { user, accessToken });
+        }
+      } catch (error) {
+        console.log(error);
+        // next(error);
+      }
+    }
+  )
 );
 //GETTING AUTHOR FROM PROVIDER & CONVERTED INTO A JSON
 passport.serializeUser(function (user, done) {
-done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser(function (user, done) {
-done(user, null);
+  done(user, null);
 });
--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 //Route
 const usersRouter = require("./users");
 const apiRouter = require("./crud");
+const oauth = require("./auth/oauth"); // ATTACHING GOOGLE STRATEGY TO PASSPORT
+const passport = require("passport");
 //Server
 const server = express();
 const port = process.env.PORT || 3007;
@@ -14,6 +16,7 @@ server.use(express.json());
 //
 server.use("/users", usersRouter);
 server.use("/deezer", apiRouter);
+server.use(passport.initialize()); //INITIALIZE PASSPORT
 //
 console.log(listEndpoints(server));
 
